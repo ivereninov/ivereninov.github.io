cf597eb32174639e82d218a3bc507689eef2f51f
Igor Vereninov <igor.vereninov@emlid.com>
2021-12-28 10:10:01 +0300

    Second iteration

--- script2.js
+++ script2.js
@@ -1,6 +1,9 @@
 window.addEventListener('load', main, false);
 function main() {
 
+  const isNumber = value => typeof value === 'number' && value === value && value !== Infinity && value !== -Infinity;
+
+
     var art = [[0, -82.6,  25.7,  157.3,  14.9,  9.80, -127.9,    9.5,   20.1,     251.6,    1541.4,     146.2],
     [24, -20.5, 146.7,   12.9, 110.7, 33.68,  -19.2,    4.7,   11.5,    4942.7,     433.2,    3729.2],
     [48, -18.9, 144.3,   12.2, 123.2, 49.37,  -18.1,    4.9,   12.8,    7126.8,     602.6,    6082.6],
@@ -29,6 +32,13 @@ function main() {
     var ml = [];
     var mva = [];
     var mz = [];
+    var naC = [];
+    var kC = [];
+    var clC = [];
+    var mosor = [];
+    var mun = [];
+    var muk = [];
+    var mucl = [];
     var na2;
     var k2;
     var cl2;
@@ -48,23 +58,23 @@ function main() {
     var na0 = 140.0;
     var k0 = 5.0;
     var cl0 = 155.0;
-    B0 = 0.0;
-    kv = 1.000;
-    na1 = 25.7;
-    k1 = 157.3;
-    cl1 = 14.9;
-    beta = 0.050;
-    gamma = 1.50;
-    pna = 0.05;
-    pk = 3.33;
-    pcl = 0.035;
-    inc = 0.001;
-    ikc = 0;
-    inkcc = 0;
-    hp = 1;
-    kb = 0;
+    var B0 = 0.0;
+    var kv = 1.000;
+    var na1 = 25.7;
+    var k1 = 157.3;
+    var cl1 = 14.9;
+    var beta = 0.050;
+    var gamma = 1.50;
+    var pna = 0.05;
+    var pk = 3.33;
+    var pcl = 0.035;
+    var inc = 0.001;
+    var ikc = 0;
+    var inkcc = 0;
+    var hp = 1;
+    var kb = 0;
 
-    // idk assignment
+    // 
     var na = na1;
     var k = k1;
     var cl = cl1;
@@ -86,7 +96,12 @@ function main() {
     var prcl;
     var h = 0.1;
     var t = 0;
+    var g;
+    var u;
     lt =  1;
+    naC.push(na*V)
+    kC.push(k*V)
+    clC.push(cl*V)
     
     var unach = -5.5;
     var ukon = 0.2;
@@ -121,22 +136,33 @@ function main() {
     }
 
     function calc() {
-        var u = solveU(na, k, cl, na0, k0, cl0, pna, pk, pcl, beta, gamma);
+        u = solveU(na, k, cl, na0, k0, cl0, pna, pk, pcl, beta, gamma);
         mt.push(t);
-        mu.push(u);
+        mu.push(u*26.7);
         mna.push(na);
         mk.push(k);
         mcl.push(cl);
         mva.push(V);
         mz.push(z);
+        if (na!=0)
+            mun.push(26.7*(u-Math.log(na0/na)));
+        else 
+            mun.push(0);
+        if (k!=0)
+            muk.push(26.7*(u-Math.log(k0/k)));
+            else 
+                muk.push(0);
+        if (cl!=0)
+            mucl.push(26.7*(-u-Math.log(cl0/cl)));
+            else 
+                mucl.push(0);
 
         var i=0;
         do {
             // solve eq for u    vichu in pascal
-            var u = solveU(na, k, cl, na0, k0, cl0, pna, pk, pcl, beta, gamma);
-            //console.log(u);
+            u = solveU(na, k, cl, na0, k0, cl0, pna, pk, pcl, beta, gamma);
             // find devariatives for Na, K, Cl   PROIZBEZ in pascal
-            var g = 1-Math.exp(u);
+            g = 1-Math.exp(u);
             if ((cl0!=0) && (k0!=0) && (na0!=0)) {
                 var jnc = inc*(na0*cl0-na*cl);
                 var jkc = ikc*(k0*cl0-k*cl);
@@ -163,27 +189,33 @@ function main() {
             mast = (k0+na0+cl0+BB-k-na-cl);
             z = (cl-na-k)/mast;
             V = A1/mast;
+            osor = (beta/gamma*na)/(ikc*k0*cl0+inkcc*na0*k0*cl0*cl0-pk*u*k0/g );
             t = t+h;
             beta = beta-kb*h;
-
             
 
             if (i%hp==0) {
                 mt.push(t);
-                mu.push(u);
+                mu.push(u*26.7);
                 mna.push(na);
                 mk.push(k);
                 mcl.push(cl);
                 mva.push(V);
                 mz.push(z);
+                naC.push(na*V);
+                kC.push(k*V);
+                clC.push(cl*V);
+                mosor.push(osor);
+                mun.push(26.7*(u-Math.log(na0/na)));
+                muk.push(26.7*(u-Math.log(k0/k)));
+                mucl.push(26.7*(-u-Math.log(cl0/cl)));
                 mast = (k0+na0+cl0+BB-k-na-cl);
-                //console.log(fna+fk-fcl);
-                //console.log(t.toFixed(2), (u*26.7).toFixed(2), na.toFixed(2), k.toFixed(2), cl.toFixed(2), (V*1000).toFixed(6), prna.toFixed(3), prk.toFixed(3), prcl.toFixed(3));
                 lt++;
             }
             i++;
         } while ((Math.abs(prna)+Math.abs(prk)+Math.abs(prcl)>1e-8) && (t<t_max))
-        Plotly.newPlot('tester', [
+
+        Plotly.newPlot('concentration', [
             {
                 x: mt,
                 y: mna,
@@ -244,87 +276,268 @@ function main() {
                     size: 6,
                 }
             },*/
-        ]);
+        ],
+        {
+            title: {
+              text:'Ion concentration',
+              font: {
+                family: 'Courier New, monospace',
+                size: 24
+              },
+              xref: 'paper',
+              x: 0.05,
+            },
+            xaxis: {
+              title: {
+                text: 'Time (min)',
+                font: {
+                  family: 'Courier New, monospace',
+                  size: 18,
+                  color: '#7f7f7f'
+                }
+              },
+            },
+            yaxis: {
+              title: {
+                text: 'mM',
+                font: {
+                  family: 'Courier New, monospace',
+                  size: 18,
+                  color: '#7f7f7f'
+                }
+              }
+            }
+        });
+        
+        Plotly.newPlot('content', [
+            {
+                x: mt,
+                y: naC,
+                mode: 'lines',
+                name: 'Na',
+                line: {
+                    color: 'red',
+                    width: 2,
+                }
+            },
+            {
+                x: mt,
+                y: kC,
+                mode: 'lines',
+                name: 'K',
+                line: {
+                    color: 'blue',
+                    width: 2,
+                }
+            },
+            {
+                x: mt,
+                y: clC,
+                mode: 'lines',
+                name: 'Cl',
+                line: {
+                    color: 'green',
+                    width: 2,
+                }
+            },
+        ],
+        {
+            title: {
+              text:'Ion content',
+              font: {
+                family: 'Courier New, monospace',
+                size: 24
+              },
+              xref: 'paper',
+              x: 0.05,
+            },
+            xaxis: {
+              title: {
+                text: 'Time (min)',
+                font: {
+                  family: 'Courier New, monospace',
+                  size: 18,
+                  color: '#7f7f7f'
+                }
+              },
+            },
+            yaxis: {
+              title: {
+                text: 'mmol/mol A',
+                font: {
+                  family: 'Courier New, monospace',
+                  size: 18,
+                  color: '#7f7f7f'
+                }
+              }
+            }
+        });
+
+        Plotly.newPlot('volume', [
+            {
+                x: mt,
+                y: mva,
+                mode: 'lines',
+                name: 'V/A',
+                line: {
+                    color: 'black',
+                    width: 2,
+                }
+            },
+        ],
+        {
+            title: {
+              text:'V/A',
+              font: {
+                family: 'Courier New, monospace',
+                size: 24
+              },
+              xref: 'paper',
+              x: 0.05,
+            },
+            xaxis: {
+              title: {
+                text: 'Time (min)',
+                font: {
+                  family: 'Courier New, monospace',
+                  size: 18,
+                  color: '#7f7f7f'
+                }
+              },
+            },
+            yaxis: {
+              title: {
+                text: 'ml/mmol A',
+                font: {
+                  family: 'Courier New, monospace',
+                  size: 18,
+                  color: '#7f7f7f'
+                }
+              }
+            }
+        });
+
+        Plotly.newPlot('U', [
+            {
+                x: mt,
+                y: mu,
+                mode: 'lines',
+                name: 'U',
+                line: {
+                    color: 'black',
+                    width: 2,
+                }
+            },
+            {
+                x: mt,
+                y: mun,
+                mode: 'lines',
+                name: 'Una',
+                line: {
+                    color: 'red',
+                    width: 2,
+                }
+            },
+            {
+                x: mt,
+                y: muk,
+                mode: 'lines',
+                name: 'Uk',
+                line: {
+                    color: 'blue',
+                    width: 2,
+                }
+            },
+            {
+                x: mt,
+                y: mucl,
+                mode: 'lines',
+                name: 'Ucl',
+                line: {
+                    color: 'green',
+                    width: 2,
+                }
+            },
+        ],
+        {
+            title: {
+              text:'U',
+              font: {
+                family: 'Courier New, monospace',
+                size: 24
+              },
+              xref: 'paper',
+              x: 0.05,
+            },
+            xaxis: {
+              title: {
+                text: 'Time (min)',
+                font: {
+                  family: 'Courier New, monospace',
+                  size: 18,
+                  color: '#7f7f7f'
+                }
+              },
+            },
+            yaxis: {
+              title: {
+                text: 'mV',
+                font: {
+                  family: 'Courier New, monospace',
+                  size: 18,
+                  color: '#7f7f7f'
+                }
+              }
+            }
+        });
+        Plotly.newPlot('osor', [
+            {
+                x: mt,
+                y: mosor,
+                mode: 'lines',
+                name: 'OSOR',
+                line: {
+                    color: 'black',
+                    width: 2,
+                }
+            },
+        ],
+        {
+            title: {
+              text:'OSOR',
+              font: {
+                family: 'Courier New, monospace',
+                size: 24
+              },
+              xref: 'paper',
+              x: 0.05,
+            },
+            xaxis: {
+              title: {
+                text: 'Time (min)',
+                font: {
+                  family: 'Courier New, monospace',
+                  size: 18,
+                  color: '#7f7f7f'
+                }
+              },
+            },
+            yaxis: {
+              title: {
+                text: '',
+                font: {
+                  family: 'Courier New, monospace',
+                  size: 18,
+                  color: '#7f7f7f'
+                }
+              }
+            }
+        });
     }
+
+
 
     calc()
-        
-
-
-    function plot(X, Na, K, Cl, width=900, height=400) {
-        let canvas = document.createElement('canvas');
-        canvas.height = height;
-        canvas.width = width;
-        canvas.style = 'border: 1px solid black;';
-        document.body.appendChild(canvas);
-        let ctx = canvas.getContext('2d');
-        let X_max = Math.max(...X);
-        let X_min = Math.min(...X);
-        let Y_max = 200//Math.max(...Na, ...K, ...Cl)*1.1;
-        let Y_min = 0//Math.min(...Na, ...K, ...Cl)-Y_max*0.1;
-        let X_scale = width/(X_max-X_min);
-        let Y_scale = height/(Y_max-Y_min);
-        ctx.beginPath();
-        ctx.moveTo(0, height+Y_min*Y_scale);
-        ctx.lineTo(width, height+Y_min*Y_scale);
-        ctx.stroke();
-        console.log(`X limits (${X_min}; ${X_max})\nY limits (${Y_min}; ${Y_max})`);
-        ctx.lineWidth = 3;
-        ctx.beginPath();
-        ctx.moveTo(X[0], height-(Na[0]-Y_min)*Y_scale);
-        for (let i=0; i<X.length; i++) {
-            if (X[i]!=NaN || Na[i]!=NaN) {
-                ctx.lineTo((X[i]-X_min)*X_scale, height-(Na[i]-Y_min)*Y_scale);
-            }
-        }
-        ctx.strokeStyle = "red";
-        ctx.stroke();
-        ctx.beginPath();
-        ctx.moveTo(X[0], height-(K[0]-Y_min)*Y_scale);
-        for (let i=0; i<X.length; i++) {
-            if (X[i]!=NaN || K[i]!=NaN) {
-                ctx.lineTo((X[i]-X_min)*X_scale, height-(K[i]-Y_min)*Y_scale);
-            }
-        }
-        ctx.strokeStyle = "blue";
-        ctx.stroke();
-        ctx.beginPath();
-        ctx.moveTo(X[0], height-(Cl[0]-Y_min)*Y_scale);
-        for (let i=0; i<X.length; i++) {
-            if (X[i]!=NaN || Cl[i]!=NaN) {
-                ctx.lineTo((X[i]-X_min)*X_scale, height-(Cl[i]-Y_min)*Y_scale);
-            }
-        }
-        ctx.strokeStyle = "green";
-        ctx.stroke();
-        ctx.lineWidth = 1;
-        ctx.beginPath();
-        ctx.moveTo(art[0][0], height-(Na[0]-Y_min)*Y_scale);
-        for (let i=0; i<art.length; i++) {
-            ctx.lineTo((art[i][0]-X_min)*X_scale, height-(art[i][2]-Y_min)*Y_scale);
-            ctx.arc((art[i][0]-X_min)*X_scale, height-(art[i][2]-Y_min)*Y_scale, 3, 0, 2*Math.PI);
-        }
-        ctx.strokeStyle = "red";
-        ctx.stroke();
-        ctx.beginPath();
-        ctx.moveTo(art[0][0], height-(K[0]-Y_min)*Y_scale);
-        for (let i=0; i<art.length; i++) {
-            ctx.lineTo((art[i][0]-X_min)*X_scale, height-(art[i][3]-Y_min)*Y_scale);
-            ctx.arc((art[i][0]-X_min)*X_scale, height-(art[i][3]-Y_min)*Y_scale, 3, 0, 2*Math.PI);
-        }
-        ctx.strokeStyle = "blue";
-        ctx.stroke();
-        ctx.beginPath();
-        ctx.moveTo(art[0][0], height-(Cl[0]-Y_min)*Y_scale);
-        for (let i=0; i<art.length; i++) {
-            ctx.lineTo((art[i][0]-X_min)*X_scale, height-(art[i][4]-Y_min)*Y_scale);
-            ctx.arc((art[i][0]-X_min)*X_scale, height-(art[i][4]-Y_min)*Y_scale, 3, 0, 2*Math.PI);
-        }
-        ctx.strokeStyle = "green";
-        ctx.stroke();
-    }
-
-    //plot(mt, mna, mk, mcl);
     
 
     form_submit.onclick = function (e) {
@@ -339,6 +552,13 @@ function main() {
         ml = [];
         mva = [];
         mz = [];
+        naC = [];
+        kC = [];
+        clC = [];
+        mosor = [];
+        mun = [];
+        muk = [];
+        mucl = [];
 
         // READING DATA
         na0 = parseFloat(input_na0.value);
@@ -357,10 +577,14 @@ function main() {
         inc = parseFloat(input_inc.value);
         ikc = parseFloat(input_ikc.value);
         inkcc = parseFloat(input_inkcc.value);
+        t_max = parseFloat(input_tmax.value);
+        if (!(isNumber(na0) && isNumber(k0) && isNumber(cl0) && isNumber(B0) && isNumber(kv) && isNumber(na1) && isNumber(k1) && isNumber(cl1) && isNumber(beta) && isNumber(gamma) && isNumber(pna) && isNumber(pk) && isNumber(pcl) && isNumber(inc) && isNumber(ikc) && isNumber(inkcc) && isNumber(t_max))) {
+          warning_div.style.display = 'inline-block';
+          throw 'BAD DATA'
+        }
         hp = 1;
         kb = 0;
 
-        // idk assignment
         na = na1;
         k = k1;
         cl = cl1;
@@ -380,6 +604,62 @@ function main() {
         h = 0.1;
         t = 0;
         lt =  1;
+        
+        naC = [na*V];
+        kC = [k*V];
+        clC = [cl*V];
+
+        if ((na2<=0) || (k2<=0) || (cl2<=0) || (V2<=0) || (V<=0) || (z>=0)) {
+          throw 'BAD INITIAL DATA';
+        }
+
         calc();
+    }
+
+    function download(filename, text) {
+        var element = document.createElement('a');
+        element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+        element.setAttribute('download', filename);
+      
+        element.style.display = 'none';
+        document.body.appendChild(element);
+      
+        element.click();
+      
+        document.body.removeChild(element);
+      }
+
+    download_result.onclick = function(e) {
+        var data = {
+            't': mt,
+            'U': mu,
+            'na': mna,
+            'k': mk,
+            'cl': mcl,
+            'V/A': mva,
+            'mun': mun,
+            'muk': muk,
+            'mucl': mucl,
+            'naC': naC,
+            'kC': kC,
+            'clC': clC,
+        }
+        var fields = Object.keys(data);
+        var result_text = fields.join(' ')+'\n\n';
+        for (var i=0; i<data['t'].length; i++) {
+            for (const key of fields) {
+                result_text += data[key][i]+' '
+            }
+            result_text = result_text.substring(0, result_text.length-1)+'\n'
+        }
+        var jnc=inc*(na0*cl0-na*cl);
+        var jkc=ikc*(k0*cl0-k*cl);
+        var jnkcc=inkcc*(na0*k0*cl0*cl0-na*k*cl*cl);
+
+        var jnce=inc*(-na*cl);
+        var jkce=ikc*(-k*cl);
+        var jnkcce=inkcc*(-na*k*cl*cl);
+        result_text += `* na0 k0 cl0 B0 kv na k cl beta gamma\n* ${na0} ${k0} ${cl0} ${B0} ${kv} ${na1} ${k1} ${cl1} ${beta} ${gamma}\n* pna pk pcl inc ikc inkcc hp kb\n* ${pna} ${pk} ${pcl} ${inc} ${ikc} ${inkcc} ${hp} ${kb}\n\n* Net_flux PUMP Channel NC KC NKCC\n* Na ${-beta*na} ${pna*u*(na*Math.exp(u)-na0)/g} ${jnc} ${0} ${jnkcc}\n* K ${beta/gamma*na} ${pk*u*(k*Math.exp(u)-k0)/g} ${0} ${jkc} ${jnkcc}\n* Cl ${0} ${pcl*u*(cl-cl0*Math.exp(u))/g} ${jnc} ${jkc} ${2*jnkcc}\n* Influx PUMP IChannel INC IKC INKCC\n* Na ${0} ${-pna*u*na0/g} ${inc*na0*cl0} ${0} ${inkcc*na0*k0*cl0*cl0}\n* K ${beta/gamma*na} ${pk*u*(-k0)/g} ${0} ${ikc*k0*cl0} ${inkcc*na0*k0*cl0*cl0}\n* Cl ${0} ${pcl*u*(-cl0*Math.exp(u))/g} ${inc*na0*cl0} ${ikc*k0*cl0} ${2*inkcc*na0*k0*cl0*cl0}\n* Efflux PUMP EChannel ENC EKC ENKCC\n* Na ${-beta*na} ${pna*u*na*Math.exp(u)/g} ${jnce} ${0} ${jnkcce}\n* K ${0} ${pk*u*k*Math.exp(u)/g} ${0} ${jkce} ${jnkcce}\n* Cl ${0} ${pcl*u*cl/g} ${jnce} ${jkce} ${2*jnkcce}\n\n* z OSOR (A/V)*1000\n* ${z} ${(beta/gamma*na)/(ikc*k0*cl0+inkcc*na0*k0*cl0*cl0-pk*u*k0/g )} ${(k0+na0+cl0+BB-k-na-cl)*1000}`
+        download('result.csv', result_text);
     }
 }
